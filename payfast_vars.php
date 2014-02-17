caae86db3cff52ae2e0ffd29c8f2441c74cdbadb
Ron Darby <ron.darby@payfast.co.za>
2014-02-17 15:58:00 +0200

    first name variable miss-spelled

--- payfast_vars.php
+++ payfast_vars.php
@@ -8,47 +8,43 @@
  **/
 function espresso_display_payfast($payment_data) 
 {
-	extract($payment_data);
-	global $wpdb;
-	include_once ('Payfast.php');
-	$myPayfast = new EE_Payfast();
-	echo '<!-- Event Espresso Payfast Gateway Version ' . $myPayfast->gateway_version . '-->';
-	global $org_options;
-	$payfast_settings = get_option('event_espresso_payfast_settings');
-	$payfast_id = empty($payfast_settings['payfast_id']) ? '' : $payfast_settings['payfast_id'];
-	$payfast_cur = empty($payfast_settings['currency_format']) ? '' : $payfast_settings['currency_format'];
-	$no_shipping = isset($payfast_settings['no_shipping']) ? $payfast_settings['no_shipping'] : '0';
-	$use_sandbox = $payfast_settings['use_sandbox'];
+    extract($payment_data);
+    global $wpdb;
+    include_once ('Payfast.php');
+    $myPayfast = new EE_Payfast();
+    echo '<!-- Event Espresso Payfast Gateway Version ' . $myPayfast->gateway_version . '-->';
+    global $org_options;
+    $payfast_settings = get_option('event_espresso_payfast_settings');
+    $payfast_id = empty($payfast_settings['payfast_id']) ? '' : $payfast_settings['payfast_id'];
+    $payfast_cur = empty($payfast_settings['currency_format']) ? '' : $payfast_settings['currency_format'];
+    $no_shipping = isset($payfast_settings['no_shipping']) ? $payfast_settings['no_shipping'] : '0';
+    $use_sandbox = $payfast_settings['use_sandbox'];
     
     $myPayfast->addField('merchant_key', $payfast_settings['payfast_merchant_key']);
-	$myPayfast->addField('merchant_id', $payfast_settings['payfast_merchant_id']);
+    $myPayfast->addField('merchant_id', $payfast_settings['payfast_merchant_id']);
     
     
-	if ($use_sandbox) 
+    if ($use_sandbox) 
     {
-		$myPayfast->enableTestMode();
-	}
-
-	$myPayfast->addField('business', $payfast_id);
-	if ($payfast_settings['force_ssl_return']) 
+        $myPayfast->enableTestMode();
+    }
+    if ($payfast_settings['force_ssl_return']) 
     {
-		$home = str_replace("http://", "https://", home_url());
-	} 
+        $home = str_replace("http://", "https://", home_url());
+    } 
     else 
     {
-		$home = home_url();
-	}
+        $home = home_url();
+    }
     
     $myPayfast->addField('return_url', $home . '/?page_id=' . $org_options['return_url'] . '&r_id=' . $registration_id. '&type=payfast');
-	$myPayfast->addField('cancel_url', $home . '/?page_id=' . $org_options['cancel_return']);
-	$myPayfast->addField('notify_url', $home . '/?page_id=' . $org_options['notify_url'] . '&id=' . $attendee_id . '&r_id=' . $registration_id . '&event_id=' . $event_id . '&attendee_action=post_payment&form_action=payment&type=payfast');
-	
+    $myPayfast->addField('cancel_url', $home . '/?page_id=' . $org_options['cancel_return']);
+    $myPayfast->addField('notify_url', $home . '/?page_id=' . $org_options['notify_url'] . '&id=' . $attendee_id . '&r_id=' . $registration_id . '&event_id=' . $event_id . '&attendee_action=post_payment&form_action=payment&type=payfast');
+    
     $event_name = $wpdb->get_var('SELECT event_name FROM ' . EVENTS_DETAIL_TABLE . " WHERE id='" . $event_id . "'");
-	$myPayfast->addField('cmd', '_cart');
-	$myPayfast->addField('upload', '1');
-	$sql = "SELECT attendee_session FROM " . EVENTS_ATTENDEE_TABLE . " WHERE id='" . $attendee_id . "'";
-	$session_id = $wpdb->get_var($sql);
-	
+    $sql = "SELECT attendee_session FROM " . EVENTS_ATTENDEE_TABLE . " WHERE id='" . $attendee_id . "'";
+    $session_id = $wpdb->get_var($sql);
+    
     $sql = "SELECT ed.event_name, a.price_option, a.fname, a.lname, dc.coupon_code_price, dc.use_percentage
             FROM " . EVENTS_ATTENDEE_TABLE . " a
             JOIN " . EVENTS_DETAIL_TABLE . " ed 
@@ -58,74 +54,69 @@ function espresso_display_payfast($payment_data)
             WHERE attendee_session = '" . $session_id . "'";
 
     $items = $wpdb->get_results($sql);
-	$coupon_amount = empty( $items[0]->coupon_code_price ) ? 0 : $items[0]->coupon_code_price;
-	$is_coupon_pct = ( !empty( $items[0]->use_percentage ) && $items[0]->use_percentage == 'Y' ) ? true : false;
-	
+    $coupon_amount = empty( $items[0]->coupon_code_price ) ? 0 : $items[0]->coupon_code_price;
+    $is_coupon_pct = ( !empty( $items[0]->use_percentage ) && $items[0]->use_percentage == 'Y' ) ? true : false;
+    
     //payfast sends through only one amount - cart total
     $cartTotal = 0;
     
     $myPayfast->addField('item_name', $items[0]->price_option . ' for ' . $items[0]->event_name . '. Attendee: '. $items[0]->fname . ' ' . $items[0]->lname);
-	
-	if (!empty($coupon_amount)) 
+    
+    if (!empty($coupon_amount)) 
     {
-		if ($is_coupon_pct) 
+        if ($is_coupon_pct) 
         {
-			$myPayfast->addField('discount_rate_cart', $coupon_amount);
-		} 
+            $myPayfast->addField('discount_rate_cart', $coupon_amount);
+        } 
         else 
         {
-			$myPayfast->addField('discount_amount_cart', $coupon_amount);
-		}
-	}
+            $myPayfast->addField('discount_amount_cart', $coupon_amount);
+        }
+    }
     $cartTotal= $payment_data['event_cost'];
     $myPayfast->addField('amount', $cartTotal);
     
-	$myPayfast->addField('currency_code', $payfast_cur);
-	$myPayfast->addField('image_url', empty($payfast_settings['image_url']) ? '' : $payfast_settings['image_url']);
-	$myPayfast->addField('no_shipping ', $no_shipping);
-	$myPayfast->addField('name_firs', $fname);
-	$myPayfast->addField('name_last', $lname);
-	$myPayfast->addField('email_address', $attendee_email);
-	$myPayfast->addField('address1', $address);
-	$myPayfast->addField('city', $city);
-	$myPayfast->addField('state', $state);
-	$myPayfast->addField('zip', $zip);
+    $myPayfast->addField('currency_code', $payfast_cur);
+    $myPayfast->addField('image_url', empty($payfast_settings['image_url']) ? '' : $payfast_settings['image_url']);
+    $myPayfast->addField('name_first', $fname);
+    $myPayfast->addField('name_last', $lname);
+    $myPayfast->addField('email_address', $attendee_email);
     $myPayfast->addField('custom_str1', $registration_id);
     $myPayfast->addField('m_payment_id', $registration_id);
-	
-	if (!empty($payfast_settings['bypass_payment_page']) && $payfast_settings['bypass_payment_page'] == 'Y') 
+    
+    if (!empty($payfast_settings['bypass_payment_page']) && $payfast_settings['bypass_payment_page'] == 'Y') 
     {
-		$myPayfast->submitPayment();
-	} 
+        $myPayfast->submitPayment();
+    } 
     else 
     {
-		if (empty($payfast_settings['button_url'])) 
+        if (empty($payfast_settings['button_url'])) 
         {
-			if (file_exists(EVENT_ESPRESSO_GATEWAY_DIR . "/payfast/payfast.png")) 
+            if (file_exists(EVENT_ESPRESSO_GATEWAY_DIR . "/payfast/payfast.png")) 
             {
-				$button_url = EVENT_ESPRESSO_GATEWAY_DIR . "/payfast/payfast.png";
-			} 
+                $button_url = EVENT_ESPRESSO_GATEWAY_DIR . "/payfast/payfast.png";
+            } 
             else 
             {
-				$button_url = EVENT_ESPRESSO_PLUGINFULLURL . "gateways/payfast/payfast.png";
-			}
-		} 
+                $button_url = EVENT_ESPRESSO_PLUGINFULLURL . "gateways/payfast/payfast.png";
+            }
+        } 
         elseif (isset($payfast_settings['button_url'])) 
         {
-			$button_url = $payfast_settings['button_url'];
-		} 
+            $button_url = $payfast_settings['button_url'];
+        } 
         else 
         {
-			$button_url = EVENT_ESPRESSO_PLUGINFULLURL . "gateways/payfast/payfast.png";
-		}
-		$myPayfast->submitButton($button_url, 'payfast');
-	}
+            $button_url = EVENT_ESPRESSO_PLUGINFULLURL . "gateways/payfast/payfast.png";
+        }
+        $myPayfast->submitButton($button_url, 'payfast');
+    }
 
-	if ($use_sandbox) 
+    if ($use_sandbox) 
     {
-    	echo '<h3 style="color:#ff0000;" title="Payments will not be processed">' . __('Payfast Debug Mode Is Turned On', 'event_espresso') . '</h3>';
-		$myPayfast->dump_fields();
-	}
+        echo '<h3 style="color:#ff0000;" title="Payments will not be processed">' . __('Payfast Debug Mode Is Turned On', 'event_espresso') . '</h3>';
+        $myPayfast->dump_fields();
+    }
 }
 
 add_action('action_hook_espresso_display_offsite_payment_gateway', 'espresso_display_payfast');
